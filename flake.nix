b5d31691d257b2111e17f2d9e2a425d605ca3d7a
daniqss <danielqueijo14@gmail.com>
2025-08-02 13:36:55 +0200

    misc(#2): follow nixos-unstable
    closes #2

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Qtile-like workspaces and monitors management for Hyprland";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
   };
 
